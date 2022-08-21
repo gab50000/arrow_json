32af447d4f1c51af5fb02dc03ab456af811bb97a
Gabriel Kabbe <gabriel.kabbe@mail.de>
2022-08-21 20:44:42 +0200

    WIP

--- main.cpp
+++ main.cpp
@@ -1,19 +1,16 @@
+#include <arrow/api.h>
 #include <arrow/filesystem/api.h>
 #include <arrow/json/api.h>
 #include <arrow/memory_pool.h>
-#include <spdlog/spdlog.h>
 
 #include <iostream>
 
 int main() {
-  auto logger = spdlog::default_logger();
   arrow::Status st;
   arrow::MemoryPool* pool = arrow::default_memory_pool();
-  logger->info("Create fs");
   auto fs =
       arrow::fs::FileSystemFromUri("file:///home/kabbe/Code/CPP/arrow_json")
           .ValueOrDie();
-  logger->info("Open file");
   auto file = fs->OpenInputFile("/home/kabbe/Code/CPP/arrow_json/test.json")
                   .ValueOrDie();
 
@@ -26,7 +23,19 @@ int main() {
       arrow::json::TableReader::Make(pool, file, read_options, parse_options)
           .ValueOrDie();
 
-  std::shared_ptr<arrow::Table> table;
   // Read table from JSON file
-  logger->info("Reading JSON");
+  auto table = reader->Read().ValueOrDie();
+
+  auto b = std::static_pointer_cast<arrow::StructArray>(
+      table->GetColumnByName("b")->chunk(0));
+
+  std::cout << "b: " << b->ToString() << "\n";
+  auto x = b->GetFieldByName("x");
+  if (x) {
+    auto val = std::static_pointer_cast<arrow::Int64Array>(x);
+    if (val) {
+      std::cout << "val\n";
+      std::cout << "val: " << val->ToString() << "\n";
+    }
+  }
 }
